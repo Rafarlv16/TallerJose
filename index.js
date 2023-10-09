ffbcad5d92d752d5e777821c6b78d7c42a2daa51
Rafarlv <rafalinaresvillareal@gmail.com>
2023-10-09 11:27:10 +0200

    Tallercomilla

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname, Inicio.html)
+    res.sendFile(__dirname, 'Inicio.html')
 })
 
 
