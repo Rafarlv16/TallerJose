655b51ebe222be55ca9e9f3b4592b681c0d9ac01
Rafarlv <rafalinaresvillareal@gmail.com>
2023-10-09 13:32:05 +0200

    TallerCorregidoFinal

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname,'Inicio.hmtl'))
+    res.sendFile(path.join(__dirname,'Inicio.html'))
 })
 
 
